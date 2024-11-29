ddcdd7799d251d6c7a62bd1e6ef9fca9fdc4f1f1
Tihana <tihana.obradovic@hotmail.com>
2024-11-29 15:40:21 +0100

    contact page

--- portfolio/src/pages/Contact/Contact.js
+++ portfolio/src/pages/Contact/Contact.js
@@ -8,13 +8,16 @@ export const Contact = () => {
         <div className={styles.contact}>
             <Header />
             <MainLayout>
-                <div className={styles.headingContact}>
+                {/* <div className={styles.headingContact}> */}
                     <h3 className={styles.h}>Contact Me </h3>
                     <p className={styles.p}>If you are ready to create the digital expirience of your dreams</p>
-                </div>
+                {/* </div> */}
                 <hr className={styles.hr} />
                 <div className={styles.contactinfo}> 
+                <div className={styles.headline}>
+                    <span className={styles.dot}></span>
                     <span className={styles.text}>CONTACT INFO</span>
+                    </div>
                     <span className={styles.number}> + 389 72 304 004 </span>
                     <span className={styles.email}>tihana.obradovic@hotmail.com</span>
                     <span className={styles.linkedin}>linkedin.com/in/tihana-obradovikj-chingarova-078835305</span>
